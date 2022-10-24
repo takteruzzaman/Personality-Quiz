084be9e5bd16ad8b37eaccb538e45866c319d215
takteruzzaman <116463326+takteruzzaman@users.noreply.github.com>
2022-10-23 23:49:26 -0400

    Add files via upload

---
+++ PersonalityQuizApp/IntroductionViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  PersonalityQuizApp
+//
+//  Created by Tyler Akteruzzaman on 10/23/22.
+//
+
+import UIKit
+
+class IntroductionViewController: UIViewController {
+    
+    @IBAction func unwindToQuizIntroduction(seque: UIStoryboardSegue) {
+        
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ PersonalityQuizApp/QuestionData.swift
@@ -0,0 +1,40 @@
+//
+//  QuestionData.swift
+//  PersonalityQuizApp
+//
+//  Created by Tyler Akteruzzaman on 10/23/22.
+//
+
+import Foundation
+
+struct Question {
+    var text: String
+    var type: ResponseType
+    var answers: [Answer]
+}
+
+enum ResponseType {
+    case single, multiple, ranged
+}
+
+struct Answer {
+    var text: String
+    var type: AnimalType
+}
+
+enum AnimalType: Character {
+    case dog = "🐶", cat = "🐱", rabbit = "🐰", turtle = "🐢"
+    
+    var definition: String {
+        switch self {
+        case .dog:
+            return "You are incredibly outgoing. You surround yourself with the people you love and enjoy activities with your friends."
+        case .cat:
+            return "Mischievous, yet milf-tempered, you enjoy doing things on your own terms."
+        case .rabbit:
+            return "You love everything that is soft. You are healthy and full of energy."
+        case .turtle:
+            return "You are wise beyond your years, and you focus on the details. Slow and steady wins the race."
+        }
+    }
+}
